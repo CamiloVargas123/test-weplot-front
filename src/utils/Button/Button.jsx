ab109cb5160f3f83fea89fc1256e86f48b6c2b9b
CamiloVargas <camilovargasingm@gmail.com>
2022-03-14 22:45:31 -0500

    created component of button

---
+++ src/utils/Button/Button.jsx
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import './Button.scss'
+
+/**
+ * 
+ * @param { secundary href value disabled onClick icon className} props 
+ * @returns button o Link si se le pasa href, primary defaul
+ */
+export default function Button(props) {
+  const {
+    secundary = false,
+    value,
+    className = "",
+    onClick,
+    disabled = false,
+    icon = false
+  } = props
+
+
+  return (
+    <button className={secundary ? `${className} secundary-button` : `${className} primary-button`} onClick={onClick} disabled={disabled}>
+      {icon ? <span><FontAwesomeIcon icon={icon} /></span> : null}
+      {value ? <span>{value}</span> : null}
+    </button>
+  )
+}
