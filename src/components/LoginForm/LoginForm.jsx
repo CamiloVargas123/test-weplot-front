c180b5cffc35612a5d3eb5ec3cf477a31a8426f4
CamiloVargas <camilovargasingm@gmail.com>
2022-03-15 15:18:17 -0500

    finish form for login user

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from '../../utils/Button/Button'
+import { useForm } from 'react-hook-form'
+import InputError from '../../utils/InputError/InputError'
+
+import './LoginForm.scss'
 
 export default function LoginForm() {
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <form>LoginForm</form>
+    <form className='loginUser' onSubmit={handleSubmit(onSubmit)}>
+      <section>
+        <label htmlFor="email">Email*</label>
+        <input className={errors.email && 'input--error'} type="email" id='email' {...register("email", { required: true })} />
+        {errors.email && <InputError />}
+      </section>
+      <section>
+        <label htmlFor="password">Contraseña*</label>
+        <input className={errors.password && 'input--error'} type="password" id='password' {...register("password", { required: true })} />
+        {errors.password && <InputError />}
+      </section>
+      <section>
+        <Button value="Acceder a Weplot" />
+      </section>
+      <div className='loginUser__link'>
+        <span>¿No tienes cuenta?</span>
+        <Link to={'/'}>Registrate aquí</Link>
+      </div>
+    </form>
   )
 }
