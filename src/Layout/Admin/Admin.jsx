7e081f0b0e22bdad02d63a7d46b108547e1fe5be
CamiloVargas <camilovargasingm@gmail.com>
2022-03-16 14:07:36 -0500

    correction style and bug

--- src/Layout/Admin/Admin.jsx
+++ src/Layout/Admin/Admin.jsx
@@ -13,15 +13,14 @@ export default function Admin() {
     })
   }, [])
 
-  console.log(allUsers)
-
   return (
     <div className='admin'>
       <div>
         <ChangeQuestionsForm />
       </div>
-      <>
-        <table class="table table-striped">
+      {allUsers && 
+      
+        <table class="admin__table table table-striped">
           <thead class="table-dark">
             <tr>
               <th scope="col">#</th>
@@ -35,19 +34,46 @@ export default function Admin() {
             {
               allUsers.map((item, idx) => {
                 return (
-                  <tr key={idx}>
-                    <th>{idx + 1}</th>
-                    <td className='admin__table--img'><img src={item.avatar} alt="avatar" /></td>
-                    <td className='admin__table--name'>{item.fname +" "+ item.lname}</td>
-                    <td>{item.email}</td>
-                    <td>{item.tel}</td>
-                  </tr>
+                  <>
+                    <tr key={idx}>
+                      <th>{idx + 1}</th>
+                      <th className='admin__table--img'><img src={item.avatar} alt="avatar" /></th>
+                      <th className='admin__table--name'>{item.fname + " " + item.lname}</th>
+                      <th>{item.email}</th>
+                      <th>{item.tel}</th>
+                    </tr>
+                    <tr>
+                      <th colspan="5">
+                        <table class="subtable table mb-0">
+                          <thead>
+                            <tr>
+                              <th></th>
+                              {item.question.map((e, i) =>  {
+                                return (
+                                  <th key={i} scope="col" className='subtable--questions'>
+                                    <p>Pregunta {i+1}</p>
+                                    <p>{e.question}</p>
+                                  </th>
+                                )
+                              })}
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                            <th></th>
+                              {item.question.map((e, i) =>  <th key={i} scope="col">{e.answer}</th>)}
+                            </tr>
+                          </tbody>
+                        </table>
+                      </th>
+                    </tr>
+                  </>
                 )
               })
             }
           </tbody>
         </table>
-      </>
+      }
     </div>
   )
 }
